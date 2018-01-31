1f291b65195f9631e510245a7c6e88b6194eef4a
nathan399 <nathanlwilliams@sky.com>
2018-01-31 12:53:33 +0000

    Rocket trail improvement

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	private void Start () {
+        GetComponent<Rigidbody>().velocity = new Vector3(9.0f, 10.0f, 0.0f);
+    }
+
+    private void Update() {
+        Vector3 vel = GetComponent<Rigidbody>().velocity;
+        transform.rotation = Quaternion.LookRotation(vel);
+        transform.Rotate(new Vector3(-90, 0, 0));
+    }
+}
