60c27a68d2e7b312339d9e18f50f0c0eb7d74cbc
nathan399 <nathanlwilliams@sky.com>
2018-02-14 12:40:02 +0000

    Explosions Increase voxel size for efficiency

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,15 +6,20 @@ public class Move : MonoBehaviour {
     [SerializeField]
     float explosionRadius;
 
-    bool collided;
+    [SerializeField]
+    GameObject explosion;
 
-	private void Start () {
+    bool collided;
+    Vector3 vel;
+
+
+    private void Start () {
         collided = false;
         //GetComponent<Rigidbody2D>().velocity = new Vector2(5.0f, 10.0f);
     }
 
     private void Update() {
-        Vector3 vel = GetComponent<Rigidbody2D>().velocity;
+        vel = GetComponent<Rigidbody2D>().velocity;
         transform.rotation = Quaternion.LookRotation(-vel);
         transform.Rotate(new Vector2(0, 90));
     }
@@ -22,6 +27,9 @@ public class Move : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision) {
         if(!collided && collision.gameObject.tag == "Ground") {
             GameObject.Find("Terrain").GetComponent<TerrainLoader>().removeVoxelsInRadius(collision.collider.transform.position, explosionRadius);
+            GameObject expl = Instantiate(explosion, collision.collider.transform.position, Quaternion.Euler(0, 0, 0));
+            Destroy(expl, 0.5f);
+
             collided = true;
             Destroy(gameObject);
         }
