c994a88583e9e209b4c4a73335c686eb0fd08a1c
matty9090 <lowe.matthew@mail.com>
2018-03-12 21:16:58 +0000

    Added menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    [SerializeField]
+    Button[] menuItems;
+
+    void Start () {
+        menuItems[0].onClick.AddListener(btn_onPlay);
+        menuItems[1].onClick.AddListener(btn_quit);
+    }
+
+    void btn_onPlay() {
+        SceneManager.LoadScene("Game");
+    }
+
+    void btn_quit() {
+        Application.Quit();
+    }
+}
