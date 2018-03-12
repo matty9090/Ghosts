b1da8d012e5418e56f54f40357706aeee6a07344
matty9090 <lowe.matthew@mail.com>
2018-03-12 22:53:55 +0000

    Add UI elements

---
+++ Assets/Scripts/WeaponUIElement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+[Serializable]
+public struct WeaponUIEl {
+    [SerializeField]
+    public GameObject prefab;
+
+    [SerializeField]
+    public Texture2D img;
+}
+
+public class WeaponUIElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public void OnPointerEnter(PointerEventData eventData) {
+        transform.GetChild(1).GetComponent<Image>().color = new Color(170.0f / 255.0f, 170.0f / 255.0f, 170.0f / 255.0f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        transform.GetChild(1).GetComponent<Image>().color = new Color(124.0f / 255.0f, 124.0f / 255.0f, 124.0f / 255.0f);
+    }
+}
---
+++ Assets/Scripts/WeaponsUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponsUI : MonoBehaviour {
+    [SerializeField]
+    GameObject uiElement;
+
+    [SerializeField]
+    Transform listElement;
+
+    [SerializeField]
+    WeaponUIEl[] weapons;
+
+	void Start () {
+        Vector3 offset = new Vector3(0.0f, 0.0f, 0.0f);
+
+        foreach (WeaponUIEl el in weapons) {
+            GameObject e = Instantiate(uiElement);
+            e.transform.SetParent(listElement);
+            e.transform.position = offset;
+            //e.transform.GetChild(2).GetComponent<Image>() = el.img;
+
+            offset += new Vector3(122.0f, 0.0f, 0.0f);
+        }
+	}
+}
