57ab906f448a51c199ab9db1ca87d5cfcb21b409
matty9090 <lowe.matthew@mail.com>
2018-03-14 11:46:44 +0000

    Hide UI on mouse up

--- Assets/Scripts/WeaponUIElement.cs
+++ Assets/Scripts/WeaponUIElement.cs
@@ -22,7 +22,6 @@ public class WeaponUIElement : MonoBehaviour, IPointerEnterHandler, IPointerExit
 
     public void OnPointerDown(PointerEventData eventData) {
         GameObject.Find("Game").GetComponent<GameController>().CurrentWorm.GetComponent<WormMovement>().missile = el.prefab;
-        GameObject.Find("Game").GetComponent<GameController>().hideUI();
 
         transform.GetChild(2).position += new Vector3(0.0f, -3.0f, 0.0f);
     }
@@ -36,6 +35,7 @@ public class WeaponUIElement : MonoBehaviour, IPointerEnterHandler, IPointerExit
     }
 
     public void OnPointerUp(PointerEventData eventData) {
+        GameObject.Find("Game").GetComponent<GameController>().hideUI();
         transform.GetChild(2).position += new Vector3(0.0f, 3.0f, 0.0f);
     }
 }
