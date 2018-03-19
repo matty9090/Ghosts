7ec0a909356883205001e6d53e11b03264468bf5
matty9090 <lowe.matthew@mail.com>
2018-03-19 18:49:43 +0000

    World bigger and camera follows worms

--- Assets/Scripts/PositionCamera.cs
+++ Assets/Scripts/PositionCamera.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PositionCamera : MonoBehaviour {
-    public float fWidth = 9.0f;
+    [SerializeField]
+    GameController controller;
 
-    void Start() {
-        float fT = fWidth / Screen.width * Screen.height;
-        fT = fT / (2.0f * Mathf.Tan(0.5f * Camera.main.fieldOfView * Mathf.Deg2Rad));
-        Vector3 v3T = Camera.main.transform.position;
-        v3T.z = -fT;
-        transform.position = v3T;
+    [SerializeField]
+    float sceneStart;
+
+    [SerializeField]
+    float sceneEnd;
+
+    void Update() {
+        Vector3 pos = new Vector3(controller.CurrentWorm.transform.position.x, transform.position.y, transform.position.z);
+
+        if(pos.x >= sceneStart && pos.x <= sceneEnd)
+            transform.position = pos;
     }
 }
