6d7a2fc3d9950614b6716e4169ddbd79c475d47b
nathan399 <nathanlwilliams@sky.com>
2018-04-24 16:24:24 +0100

    Changed meteor targeting

--- Assets/Scripts/Weapons/Meteor.cs
+++ Assets/Scripts/Weapons/Meteor.cs
@@ -19,11 +19,13 @@ public class Meteor : MonoBehaviour, Crosshair {
     GameObject explosion;
 
     bool collided;
+    bool explode;
     Vector3 vel;
-
+    Vector3 targetPosition;
 
     private void Start() {
         collided = false;
+        explode = false;
         GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.5f);
     }
 
@@ -31,9 +33,14 @@ public class Meteor : MonoBehaviour, Crosshair {
         vel = GetComponent<Rigidbody2D>().velocity;
         transform.rotation = Quaternion.LookRotation(-vel);
         transform.Rotate(new Vector3(90, 0, 0));
+
+        if ((transform.position - targetPosition).magnitude < 1.0f)
+            GetComponent<CircleCollider2D>().enabled = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        
+
         if (!collided && (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Player")) {
             GameObject.Find("Terrain").GetComponent<TerrainLoader>().removeVoxelsInRadius(collision.collider.transform.position, explosionRadius);
             GameObject expl = Instantiate(explosion, collision.collider.transform.position, Quaternion.Euler(0, 0, 0));
@@ -79,6 +86,11 @@ public class Meteor : MonoBehaviour, Crosshair {
         }
     }
 
+    public void getTargetPos(Vector3 target)
+    {
+        targetPosition = target;
+    }
+
     public bool canMove() {
         return false;
     }
@@ -102,9 +114,10 @@ public class Meteor : MonoBehaviour, Crosshair {
             Vector2 crosshairPosition = crosshair.transform.position - tmp;
 
             if (game.canFire) {
-                Instantiate(gameObject, tmp, Quaternion.LookRotation(crosshairPosition));
+                GameObject meteor = Instantiate(gameObject, tmp, Quaternion.LookRotation(crosshairPosition));
+                meteor.GetComponent<Meteor>().getTargetPos(crosshair.transform.position);
 
-                if(game.Timer > 10.9f)
+                if (game.Timer > 10.9f)
                     game.Timer = 10.9f;
             }
 
